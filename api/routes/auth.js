b1edfdc7d90539ac7fa3e3f6035d4208f6a899d5
2019000010081 <2019000010081@seu.edu.bd>
2022-10-29 01:02:11 +0600

    user registration method added

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,7 +3,7 @@ import { login, register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/register", register);
-router.get("/login", login);
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
