dcad2fd6cb0009c259e88acb3606c8696d2bc8ec
2019000010081 <2019000010081@seu.edu.bd>
2022-10-28 23:47:33 +0600

    hotel api create read update delete method added

---
+++ api/controllers/auth.js
@@ -0,0 +1,2 @@
+export const register = async (req, res, next) => {};
+export const login = async (req, res, next) => {};
--- api/index.js
+++ api/index.js
@@ -1,7 +1,53 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
+
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 8800;
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnect", function () {
+  console.log("mongodb disconnected");
+});
+
+app.get("/api", (req, res) => {
+  res.send("Hello from booking app");
+});
+
+// middleware(s)
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
 app.listen(port, () => {
+  connect();
   console.log("Connected to booking app backend on port", port);
 });
